d258fca66b103dfeaef16ad59123b61e51775b30
eltrenzoxd <94225544+eltrenzoxd@users.noreply.github.com>
2023-06-26 16:25:49 +0000

    arreglos

--- carpeta por si acaso/entradadedatos.py
+++ carpeta por si acaso/entradadedatos.py
@@ -9,9 +9,9 @@
 #print ("tu peso es de:", peso )
 #El + es para unir una variable con otra
 #lo anterior es lo mismo que:
-nombre = input('¿Cual es tu nombre?'\n)
-edad = int(input('¿Cual es tu edad?'\n))
-peso = float(input(¿Cual es tu peso?\n))
+nombre = input("¿Cual es tu nombre?"\n)
+edad = int(input("¿Cual es tu edad?"\n))
+peso = float(input("¿Cual es tu peso?"\n))
 print ('Mucho gusto '+nombre)
 print (Tu edad es de:, edad)
 print ('Tu peso es de', peso)
