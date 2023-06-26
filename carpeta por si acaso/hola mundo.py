b5877e8fcf8988b2a528a70893ba556fe17bc8ad
eltrenzoxd <94225544+eltrenzoxd@users.noreply.github.com>
2023-06-26 10:12:41 -0400

    Create hola mundo.py

---
+++ carpeta por si acaso/hola mundo.py
@@ -0,0 +1,16 @@
+print ("hola mundo")
+
+nombre = 'young petrus'
+lastname = 'dame chicles y chocolates'
+age = 69420
+
+print (nombre)
+print (lastname)
+print (age)
+
+age = 43
+print (age)
+
+#si es todo mayuscula significa que es una constante (no cambia)
+PI = 3.14 
+MAYORDEEDAD = 18
