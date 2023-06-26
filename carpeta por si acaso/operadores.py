2e584a723749103efa202c43fffd316fcf470430
eltrenzoxd <94225544+eltrenzoxd@users.noreply.github.com>
2023-06-26 10:52:18 -0400

    Create operadores.py

---
+++ carpeta por si acaso/operadores.py
@@ -0,0 +1,31 @@
+#edad=69
+#nombre='pate baby'
+#trabajo='ser un flojo de mierda'
+
+#print (edad)
+#print (nombre)
+#print (trabajo)
+#todo lo anterior es lo mismo que:
+edad,nombre,trabajo = 69,'pate baby','ser un flojo de mierda'
+
+print (edad)
+print (nombre)
+print (trabajo)
+#variables para operadores:
+a = 2
+b = 7
+#operadores relacionales:
+mayor = a>b
+menor = b<a
+mayor_igual = a >= b
+menor_igual = a <= b
+igual = a == b
+diferente = a != b
+
+print (mayor)
+print (menor)
+print (mayor_igual)
+print (menor_igual)
+print (igual)
+print (diferente)
+
