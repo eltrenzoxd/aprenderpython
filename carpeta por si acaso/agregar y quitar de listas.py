7504e4890325d44b3ebdef82fbe4e8e21eb9c050
eltrenzoxd <94225544+eltrenzoxd@users.noreply.github.com>
2023-06-26 22:18:34 +0000

    listas

---
+++ carpeta por si acaso/agregar y quitar de listas.py
@@ -0,0 +1,13 @@
+lista = ["tu vieja", "tu viejo", "tu prima", "tu hermana",
+         "tus muertos", "constantino papadopulos"]
+print(lista)
+lista.remove("tu viejo")
+print(lista)
+
+lista.append("constantino papadopulos")
+print(lista)
+# para vaciar la lista se puede ejecutar:
+'''
+lista.clear
+print(lista)
+'''
