873892e642a8b62180416cf4c9bde2079674f34f
eltrenzoxd <94225544+eltrenzoxd@users.noreply.github.com>
2023-06-27 23:47:02 +0000

    yippe

---
+++ carpeta por si acaso/matriz.py
@@ -0,0 +1,10 @@
+lista1=[69,420]
+lista2=[69420,42069]
+
+matriz=[lista1,lista2]
+print(matriz)
+print("elemento de matriz")
+print(matriz[1][0])
+#el 1er valor es por fila (horizontal): fila 1 [] []
+#                                       fila 2 [] []
+# y el segundo por columna (vertical)
---
+++ carpeta por si acaso/tuplas.py
@@ -0,0 +1,4 @@
+# las tuplas se usan cuando tienes una lista que no se  va a modificar en nada
+Gente_que_vive_en_mi_casa = ("yo", "mi papá", "mi mamá", "mi hermano")
+print(Gente_que_vive_en_mi_casa[0])
+print(len(Gente_que_vive_en_mi_casa))
